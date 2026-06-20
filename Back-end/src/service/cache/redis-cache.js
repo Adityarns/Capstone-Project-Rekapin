92885c6219a380ef70dc31bcd4b14d86a06fae8f
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2026-06-20 14:25:01 +0700

    refactor: change upstash redis to localy in vps

--- Back-end/src/service/cache/redis-cache.js
+++ Back-end/src/service/cache/redis-cache.js
@@ -4,8 +4,8 @@ class CacheService {
   constructor() {
     // Upstash menggunakan URL dan TOKEN, bukan host dan port
     this._client = new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+      url: process.env.REDIS_URL,
+      token: process.env.REDIS_TOKEN,
     });
   }
 
